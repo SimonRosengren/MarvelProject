acacb4b976e2c41fcdb20cb0705b8dc4a76b75da
xzorcious <simonrosengren123@gmail.com>
2017-04-05 16:19:26 +0200

    Adding second hero

--- client/static/marvelapi.js
+++ client/static/marvelapi.js
@@ -20,22 +20,36 @@ function getHero(event) {
   req.addEventListener('load', function () {
     if (req.status >= 200 && req.status < 400) {
 
-      //getting a random number between 0 and 29
+      //getting two random numbers between 0 and 29
       var randomInt = Math.floor(Math.random() * 29)
+      var randomInt2 = Math.floor(Math.random() * 29)
 
       //Prints out the name in the HTML
       var result = JSON.parse(req.responseText)
       console.log(result.data.results[randomInt])
       document.getElementById('name').textContent = result.data.results[randomInt].name
-      
+      document.getElementById('name2').textContent = result.data.results[randomInt2].name
+
       //Show the picture in the HTML
       var img = document.createElement('img')
       img.className += "pic"
       img.src = result.data.results[randomInt].thumbnail.path + '/standard_fantastic.jpg'
       document.getElementById('pic').appendChild(img)
 
+      var img2 = document.createElement('img')
+      img2.className += "pic"
+      img2.src =  result.data.results[randomInt2].thumbnail.path + '/standard_fantastic.jpg'
+      document.getElementById('pic2').appendChild(img2)
+
       //Prints out the description of the hero in the HTML
       document.getElementById('info').textContent = result.data.results[randomInt].description
+      if (result.data.results[randomInt].description === "") {
+        document.getElementById('info').textContent = "No description, sorry! :("
+      }
+      document.getElementById('info2').textContent = result.data.results[randomInt2].description
+      if (result.data.results[randomInt2].description === "") {
+        document.getElementById('info2').textContent = "No description, sorry! :("
+      }
       console.log(img)
     }
     event.preventDefault()
